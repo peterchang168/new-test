212352ff551d2f248dcc55695379ad571b7ddd56
Peter L Chang (RD-TW) <peterl_chang@trend.com.tw>
2015-12-09 16:36:13 +0800

    update unittest.sh

--- test/agent2/unittest.sh
+++ test/agent2/unittest.sh
@@ -2,7 +2,7 @@
 mkdir -p /tmp/agent2_test
 cp ${PWD}/bin/agent2.py /tmp/agent2_test
 cp ${PWD}/test/unittest/unittest_agent2.py /tmp/agent2_test
-mkdir -p /tmp/agent2_test/report
-nosetests -v -s -x --with-xunit --xunit-file=$CIRCLE_TEST_REPORTS/agent2_unit_result.xml --cover-erase --with-coverage --cover-package=agent2 -w /tmp/agent2_test/ unittest_agent2.py
-coverage xml -o $CIRCLE_TEST_REPORTS/agent2_coverage.xml /tmp/agent2_test/agent2.py
+mkdir -p /tmp/agent/report
+nosetests -v -s -x --with-xunit --xunit-file=/tmp/agent/report/agent2_unit_result.xml --cover-erase --with-coverage --cover-package=agent2 -w /tmp/agent2_test/ unittest_agent2.py
+coverage xml -o /tmp/agent/report/agent2_coverage.xml /tmp/agent2_test/agent2.py
 
